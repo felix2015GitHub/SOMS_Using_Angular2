f98d895b8330d9342afdff1f8b2bbc6ddfa4d69b
Felix <felix_cheng@syntrontech.com>
2016-07-03 02:09:13 +0800

    - add home page. - add table to sub page.

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,9 +1,19 @@
 import { provideRouter, RouterConfig }  from '@angular/router';
+import { HomeComponent } from './home.component';
 import { MembersComponent } from './members.component';
 import { ProductsComponent } from './products.component';
 import { OrdersComponent } from './orders.component';
 
 const routes: RouterConfig = [
+  {
+    path: '',
+    redirectTo: '/home',
+    terminal: true
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
   {
     path: 'members',
     component: MembersComponent
